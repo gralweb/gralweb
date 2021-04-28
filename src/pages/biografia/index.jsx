1ff6e4c5f4c0c0a47cf5dba4517bb7faef208426
gralweb <gralweb21@gmail.com>
2021-04-28 14:03:14 -0430

    mejorando el contenido de la biografia - con mas sentido

--- src/pages/biografia/index.jsx
+++ src/pages/biografia/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // Componentes
 import { ASSETS_URL } from './../../components/constans'
@@ -26,7 +26,7 @@ const RenderBiografia = ({ headerLocation }) => {
                         Hola, mi nombre es George Alviarez y soy desarrollador web. Soy de Venezuela - Estado Lara y tengo actualmente 19 años de edad. Me gustan los comics, el baloncesto, los video juegos y más que todo la tecnología.
                     </p>
                     <p className='app-bio-text'>
-                        Comencé en el mundo del desarrollo web a los 16 años y desde entonces sólo he estado aprendiendo sobre el tema para poder trabajar con esta rama de la tecnología. Hoy en día me siento capaz de trabajar con ello, por esa razón he hecho este portafolio donde muestro algunas de las cosas con las que he practicado para pulir mis conocimientos en el desarrollo web. La mayoría de estos proyectos están basados en diseños de inspiración que he buscado en diferentes sitios en la web para ponerme retos y mejorar cada vez más.
+                        Comencé en el mundo del desarrollo web a los 17 años y desde entonces sólo he estado aprendiendo sobre el tema para poder trabajar con esta rama de la tecnología. Hoy en día me siento un 85% capaz de trabajar con ello, el otro 15% meda miedo o nervios porque no tengo experiencia laboral, por esa razón he hecho este portafolio donde muestro algunas de las cosas que he hecho y siento que demuestran el conocimiento que he adquirido en este tiempo.
                     </p>
                     <p className='app-bio-text'>
                        No me considero un experto, pero tengo la capacidad para encontrar soluciones a fallos que se encuentren en el proceso de desarrollo de un sitio web, y siento que siempre hay algo más para aprender en esta rama ya que es muy amplia y se encuentra en constante desarrollo.
@@ -34,7 +34,7 @@ const RenderBiografia = ({ headerLocation }) => {
                 </div>
             </div>
         </div>
-  	);
+  	)
 }
 
-export default RenderBiografia;
+export default RenderBiografia
