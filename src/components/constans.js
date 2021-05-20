233553b620dd11a9ca6f7f897295feb66952539a
gralweb <gralweb21@gmail.com>
2021-05-20 08:06:16 -0430

    uploadTest - Cambiando direccion url para los datos - en produccion

--- src/components/constans.js
+++ src/components/constans.js
@@ -1,27 +1,19 @@
-const API_URL_CARTS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/articulos.php'
+// const API_URL_CARTS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/articulos.php'
 
-const API_URL_PRESENTACION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
+// const API_URL_PRESENTACION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
 
-const API_URL_CONTACTOS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
+// const API_URL_CONTACTOS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
 
-const API_URL_SUSCRIPCION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
+// const API_URL_SUSCRIPCION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
 
 // URL PARA OTROS DISPOSITIVOS - O - PRODUCCIÓN
-// const API_URL_CARTS = 'http://192.168.5.104/programacion/gralweb/portafolio_backend/ajax/articulos.php'
+const API_URL_CARTS = 'https://gralweb.000webhostapp.com/ajax/articulos.php'
 
-// const API_URL_PRESENTACION = 'http://192.168.5.104/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
+const API_URL_PRESENTACION = 'https://gralweb.000webhostapp.com/ajax/presentacion.php'
 
-// const API_URL_CONTACTOS = 'http://192.168.5.104/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
+const API_URL_CONTACTOS = 'https://gralweb.000webhostapp.com/ajax/setMensajes.php'
 
-// const API_URL_SUSCRIPCION = 'http://192.168.5.104/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
-
-// const API_URL_CARTS = 'http://192.168.5.102/programacion/gralweb/portafolio_backend/ajax/articulos.php'
-
-// const API_URL_PRESENTACION = 'http://192.168.5.102/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
-
-// const API_URL_CONTACTOS = 'http://192.168.5.102/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
-
-// const API_URL_SUSCRIPCION = 'http://192.168.5.102/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
+const API_URL_SUSCRIPCION = 'https://gralweb.000webhostapp.com/ajax/setSuscripcion.php'
 
 const ASSETS_URL = process.env.PUBLIC_URL
 
