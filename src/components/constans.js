d85b1f294fbd0065d7425aa2a95ba81de5c46d23
gralweb <gralweb21@gmail.com>
2021-12-05 08:35:38 -0430

    new - cambio en las url para produccion

--- src/components/constans.js
+++ src/components/constans.js
@@ -1,19 +1,19 @@
-const API_URL_CARTS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/articulos.php'
+// const API_URL_CARTS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/articulos.php'
 
-const API_URL_PRESENTACION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
+// const API_URL_PRESENTACION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/presentacion.php'
 
-const API_URL_CONTACTOS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
+// const API_URL_CONTACTOS = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setMensajes.php'
 
-const API_URL_SUSCRIPCION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
+// const API_URL_SUSCRIPCION = 'http://localhost/programacion/gralweb/portafolio_backend/ajax/setSuscripcion.php'
 
 // URL PARA OTROS DISPOSITIVOS - O - PRODUCCIÓN
-// const API_URL_CARTS = 'https://gralweb.000webhostapp.com/ajax/articulos.php'
+const API_URL_CARTS = 'https://gralweb.000webhostapp.com/static/ajax/articulos.php'
 
-// const API_URL_PRESENTACION = 'https://gralweb.000webhostapp.com/ajax/presentacion.php'
+const API_URL_PRESENTACION = 'https://gralweb.000webhostapp.com/static/ajax/presentacion.php'
 
-// const API_URL_CONTACTOS = 'https://gralweb.000webhostapp.com/ajax/setMensajes.php'
+const API_URL_CONTACTOS = 'https://gralweb.000webhostapp.com/static/ajax/setMensajes.php'
 
-// const API_URL_SUSCRIPCION = 'https://gralweb.000webhostapp.com/ajax/setSuscripcion.php'
+const API_URL_SUSCRIPCION = 'https://gralweb.000webhostapp.com/static/ajax/setSuscripcion.php'
 
 const ASSETS_URL = process.env.PUBLIC_URL
 
